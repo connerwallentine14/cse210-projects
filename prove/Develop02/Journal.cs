93a9a14073c94b14ef0f8556f2f2ef18756739fa
Conner Wallentine <connerwallentine@gmail.com>
2026-01-31 21:40:19 -0700

    git

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                writer.WriteLine($"{entry._date}|{entry._prompt}|{entry._response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+            Entry entry = new Entry(parts[0], parts[1], parts[2]);
+            _entries.Add(entry);
+        }
+    }
+}

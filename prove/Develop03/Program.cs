f53899d1746a6ee04636e44764166d1a0093cafb
Conner Wallentine <connerwallentine@gmail.com>
2026-02-15 20:55:21 -0800

    Encapsulation Scripture Memorization

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        List <Scripture> scriptures = new List<Scripture>
+        {
+            new Scripture(
+                new Reference("John", 3, 16),
+                "For God so loved the world that he gave his only begotten Son that whosoever believeth in him should not perish but have everlasting life"
+            ),
+            new Scripture(
+                new Reference("Proverbs", 3, 5, 6),
+                "Trust in the Lord with all thine heart and lean not unto thine own understanding In all thy ways acknowledge him and he shall direct thy paths"
+            ),
+        }; 
+
+        Random rand = new Random();
+        Scripture scripture = scriptures[rand.Next(scriptures.Count)];
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+            Console.Write("Press Enter to continue nor type 'quit': ");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            if (scripture.IsCompletelyHidden())
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(3);
+        }
+        
     }
 }
